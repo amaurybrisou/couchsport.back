79ec5e2f70aa59f53db5253f0a4c349979f139aa
amaurybrisou <github.com@puzzledge.org>
2019-01-07 19:18:40 +0000

    home page improvements

--- public/src/components/Home.vue
+++ public/src/components/Home.vue
@@ -2,11 +2,21 @@
   <v-container fill-height>
     <!-- <v-layout class="frontend-content" row wrap align-center> -->
     <v-card class="frontend-content">
-      <v-toolbar color="secondary" dense  >
+      <v-toolbar color="secondary" dense>
         <v-toolbar-title>CoSport</v-toolbar-title>
       </v-toolbar>
       <v-card-text class="text-content">
         <h3 class="subheading">Travel, make friends, practicing your favorite sports together !</h3>
+        <v-divider></v-divider>
+        <div class="body-1 mt-2">Begin the tour browsing available spots in the
+          <router-link to="/explore">
+            <span color="accent">explore</span>
+          </router-link> page or create your
+          <router-link to="/signup">
+            <span color="accent">account</span>
+          </router-link>
+          and promote your own spot!
+        </div>
       </v-card-text>
       <!-- <v-flex class="text-xs-center"> -->
       <!-- <h1 class="display-3 font-weight-thin mb-3">CoSport</h1> -->
@@ -28,6 +38,6 @@ export default {
   position: absolute;
   top: 10vh;
   left: 10vh;
-  background-color: rgba(255, 255, 255, .8) !important;
+  background-color: rgba(255, 255, 255, 0.8) !important;
 }
 </style>
