49ca9041ac148096d68f57a0bcdd59c5b4baa6a3
amaurybrisou <github.com@puzzledge.org>
2019-01-08 09:12:05 +0000

    fix baseURL syntax

--- public/config/dev.env.js
+++ public/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  baseURL: "http://localhost:8080/api"
+  baseURL: '"http://localhost:8080/api"'
 })
